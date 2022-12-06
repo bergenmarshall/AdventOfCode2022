3a59f1864ae111333adba775a473cff72e5a2ea9
Bergen Marshall <bergenmarshall34@gmail.com>
2022-12-05 23:36:22 -0600

    Day 6

---
+++ day6.py
@@ -0,0 +1,21 @@
+f = open('day6-input.txt','r')
+line = f.readline()
+line = line.rstrip()
+
+list = ['X','X','X','X','X','X','X','X','X','X','X','X','X']
+
+spot = 0
+
+for char in line:
+    spot += 1
+    if len(set(list)) == len(list) and list[0] != 'X':
+        print(list)
+        print(spot)
+        break
+    else:
+        list = [char] + list[:-1]
+
+
+
+
+f.close()
